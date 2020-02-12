9737c03cc9ccfdc5022e0c4a7d1b8d5f886c4085
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2020-02-12 14:24:05 +0100

    Add files via upload

---
+++ splitbline.py
@@ -0,0 +1,24 @@
+def split(inputFile,numParts,outputName):
+    with open(inputFile) as openInputFile:
+        content = openInputFile.readlines()
+
+    nbTotalLine = len(content)
+    nbLine = nbTotalLine // numParts
+    firstLine = 0
+    lastLine = nbLine
+
+
+    for i in range(1,numParts+1):
+        if '.' in outputName:
+            outputNameSplit=outputName.split('.')
+            openOutputFile=open(outputNameSplit[0]+str(i)+'.'+outputNameSplit[1],'a')
+        else:
+            openOutputFile=open(outputName+str(i),'a')
+
+        openOutputFile.writelines(content[firstLine:lastLine])
+        firstLine+=nbLine
+        lastLine+=nbLine
+        openOutputFile.close()
+
+
+#split('test.txt',3,'file.txt')
