682870862ead1f5ac0e559f5758d9118e0ca34e0
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2023-02-15 16:48:06 +0100

    Create cme_output_parser.py

---
+++ cme_output_parser.py
@@ -0,0 +1,19 @@
+f = open('ntds.txt', 'r').readlines()
+hashs = []
+
+print('\n====    USERS AND HASHS    ====\n')
+for i in range(len(f)):
+    user = f[i].split(':')[0].split('\\')
+    hash = f[i].split(':')[3]
+
+    hashs.append(hash)
+
+    if len(user) == 2:
+        print(f"{user[1]}:{hash}")
+    else:
+        print(f"{user}:{hash}")
+
+print('\n====    DUPLICATE HASHS    ====\n')
+for hash in set(hashs):
+    if hashs.count(hash) > 1:
+        print({hash:hashs.count(hash)})
